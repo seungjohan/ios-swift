58e35aa128c7691604e8a1b45790a2820e5209da
seungjohan <cmostrillion@gmail.com>
2022-12-08 15:01:04 +0900

    gather all the projects in one repo

---
+++ iampoor/iampoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iampoor
+//
+//  Created by seungjo han on 2022/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
